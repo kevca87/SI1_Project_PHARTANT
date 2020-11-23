418be4eebe9ba2e870301902a51e053062e84edb
kevca87 <kevcespedesarancibia87@gmail.com>
2020-11-22 22:01:32 -0400

    Producto añadido, stock en proceso

---
+++ PHARTANT/Dal/ProductMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using PHARTANT.Models;
+using System.Data.OleDb;
+
+
+namespace PHARTANT.Dal
+{
+    public class ProductMapper
+    {
+        private DataAccess.DataAccess _dataAccess;
+        private DataAccess.DataAccess DataAccess
+        {
+            get { return _dataAccess; }
+        }
+        public ProductMapper(DataAccess.DataAccess da)
+        {
+            _dataAccess = da;
+        }
+    }
+}
